068b250e7cd85eaa25b9e021ba9083b3e85343ad
Alexandre Ferreira de Lima <alexandre.ferreira1445@gmail.com>
2024-07-08 23:46:30 -0300

    ♻️ Improve create_result_folder function

--- src/commands/builder/make_result_folder.py
+++ src/commands/builder/make_result_folder.py
@@ -3,7 +3,7 @@ import pandas as pd
 from rich.console import Console
 
 from config.config import settings, ROOT_DIR
-from src.helpers.pandas_helper import read_csv_file, save_df_txt
+from src.helpers.pandas_helper import concatenate_csv_files, read_csv_file, save_df_txt
 from src.helpers.path_helper import get_all_files, make_dir, update_dir
 
 
@@ -32,12 +32,5 @@ def make_result_folder_command():
 
 
 def create_result_folder():
-    translation_files = get_all_files(TRANSLATION_FOLDER_PATH)
-    result_df = pd.DataFrame()
-    
-    for file_path in translation_files:
-        df = read_csv_file(file_path)
-        result_df = pd.concat([result_df, df['translation']])
-    
-    result_df = result_df.rename(columns={0: 'translation'})
-    save_df_txt(result_df['translation'], f'{RESULT_FOLDER_PATH}\\{settings.FILES.result_file_name}')
+    df = concatenate_csv_files(TRANSLATION_FOLDER_PATH)
+    save_df_txt(df['translation'], f'{RESULT_FOLDER_PATH}\\{settings.FILES.result_file_name}')
